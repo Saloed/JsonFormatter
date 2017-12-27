52b381002df56d00855f8ce3a074f0a0f1d6d86a
saloed <napster_1997@mail.ru>
2017-12-27 18:04:52 +0300

    configure jenkins build

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,7 +3,6 @@ node {
       git 'https://github.com/Saloed/JsonFormatter'
    }
    stage('Build') {
-   	  sh 'docker build -f jenkins.Dockerfile .'
       sh  './gradlew docker'
    }
 }
--- run_jenkins_docker.sh
+++ run_jenkins_docker.sh
@@ -1,1 +1,4 @@
-docker run -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock -v $PWD/jenkins_data:/var/jenkins_home jenkins_ci
+docker run -p 8080:8080 -p 50000:50000 \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v $PWD/jenkins_data:/var/jenkins_home \
+--rm -u root h1kkan/jenkins-docker:lts
